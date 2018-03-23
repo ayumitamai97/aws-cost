8158da2d0db6659332a101e0b435b32a39bdd926
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-23 12:54:08 +0900

    added config

--- s3.rb
+++ s3.rb
@@ -1,8 +1,12 @@
 require "aws-sdk"
+require "aws-sdk-s3"
 require "json"
 
-creds = JSON.load(File.read("../Documents/credentials/aws_credentials.json"))# credentialsはファイル分ける
-s3 = AWS::S3.new(
-  :access_key_id => creds["Access key ID"],
-  :secret_access_key => creds["Secret access key"]
-)
+creds = JSON.load(File.read("../Documents/credentials/aws_credentials.json")) # credentialsはファイル分ける
+Aws.config.update({
+  region: "ap-northeast-1",
+  credentials: Aws::Credentials.new(creds["Access key ID"], creds["Secret access key"])
+})
+s3 = Aws::S3::Client.new
+resp = s3.list_buckets
+resp.buckets.map(&:name)
