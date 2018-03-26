844975d62443a6b0cb8dfafc81b575070aea83ec
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-26 17:49:19 +0900

    コード規約一部適用

--- costexplorer.rb
+++ costexplorer.rb
@@ -35,33 +35,35 @@ ce = Aws::CostExplorer::Client.new
 # })
 #
 # values = []
-#
-# for num in 0..30 # サービス31個
-#    values << services[0][num].value
-# end
-# puts values
 
-resp = ce.get_cost_and_usage(params={
-  time_period: {
-    start: start_day, # required
-    end: end_day, # required
-  },
-  granularity: "MONTHLY", # accepts DAILY, MONTHLY
-  filter: {
-    dimensions: {
-      key: "SERVICE", # accepts AZ, INSTANCE_TYPE, LINKED_ACCOUNT, OPERATION, PURCHASE_TYPE, REGION, SERVICE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY
-      # values: ["AWS S3 (Simple Storage Service)"] # => nil
-      values: ["AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "Amazon CloudFront", "Amazon CloudSearch", "Amazon DynamoDB", "Amazon EC2 Container Registry (ECR)", "Amazon ElastiCache", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Relational Database Service", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon Simple Storage Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"],
+for num in 0..30 # サービス31個
+   values << services[0][num].value
+end
+puts values
+
+resp = ce.get_cost_and_usage(
+  params={
+    time_period: {
+      start: start_day, # required
+      end: end_day, # required
     },
-  },
-  metrics: ["BlendedCost"],
-  group_by: [
-    {
-      type: "DIMENSION", # accepts DIMENSION, TAG
-      key: "SERVICE",
+    granularity: "MONTHLY", # accepts DAILY, MONTHLY
+    filter: {
+      dimensions: {
+        key: "SERVICE", # accepts AZ, INSTANCE_TYPE, LINKED_ACCOUNT, OPERATION, PURCHASE_TYPE, REGION, SERVICE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY
+        # values: ["AWS S3 (Simple Storage Service)"] # => nil
+        values: ["AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "Amazon CloudFront", "Amazon CloudSearch", "Amazon DynamoDB", "Amazon EC2 Container Registry (ECR)", "Amazon ElastiCache", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Relational Database Service", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon Simple Storage Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"],
+      },
     },
-  ],
-})
+    metrics: ["BlendedCost"],
+    group_by: [
+      {
+        type: "DIMENSION", # accepts DIMENSION, TAG
+        key: "SERVICE",
+      },
+    ],
+  }
+)
 
 historical_all = [] # 全サービスのHistorical Total
 
