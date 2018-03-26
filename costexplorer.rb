2ee6967ba2ba3d9cdf25f05f1c30119db4bf66c6
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-26 19:39:17 +0900

    puts only required forecast costs

--- costexplorer.rb
+++ costexplorer.rb
@@ -42,20 +42,21 @@ responses = ce.get_cost_and_usage(
   params={
     time_period: {
       start: start_day, # required
-      end: end_day, # required
+      end: end_day # required
     },
     granularity: "MONTHLY", # accepts DAILY, MONTHLY
     filter: {
       dimensions: {
         key: "SERVICE", # accepts AZ, INSTANCE_TYPE, LINKED_ACCOUNT, OPERATION, PURCHASE_TYPE, REGION, SERVICE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY
-        values: ["AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "Amazon CloudFront", "Amazon CloudSearch", "Amazon DynamoDB", "Amazon EC2 Container Registry (ECR)", "Amazon ElastiCache", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Relational Database Service", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon Simple Storage Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"],
+        # values: ["AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "Amazon CloudFront", "Amazon CloudSearch", "Amazon DynamoDB", "Amazon EC2 Container Registry (ECR)", "Amazon ElastiCache", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Relational Database Service", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon Simple Storage Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"] # all services
+        values: ["Amazon Simple Storage Service", "Amazon EC2 Container Registry (ECR)", "Amazon CloudSearch", "Amazon Relational Database Service", "Amazon DynamoDB", "Amazon CloudFront", "Amazon ElastiCache" ] # required forecast costs
       },
     },
     metrics: ["BlendedCost"],
     group_by: [
       {
         type: "DIMENSION", # accepts DIMENSION, TAG
-        key: "SERVICE",
+        key: "SERVICE"
       },
     ],
   }
@@ -67,7 +68,7 @@ forecast_all = [] # 全サービスのForecast
 responses.results_by_time[0]["groups"].each do |struct| # struct は object "Aws::CostExplorer::Types::GetDimensionValuesResponse"
 
   historical = struct["metrics"]["BlendedCost"].amount.to_f # 各サービスのHistorical Total
-  puts "Historical Total: " + struct.keys[0] + ": " + historical.to_s
+  # puts "Historical Total: " + struct.keys[0] + ": " + historical.to_s
 
   forecast = historical * (last_day - past_days) / past_days
   puts "Forecast Total: " + struct.keys[0] + ": " + forecast.to_s
@@ -77,5 +78,5 @@ responses.results_by_time[0]["groups"].each do |struct| # struct は object "Aws
 
 end
 
-puts "Historical Total Cost in All Services: " + historical_all.inject{ |sum, i| sum + i }.to_s
+# puts "Historical Total Cost in All Services: " + historical_all.inject{ |sum, i| sum + i }.to_s
 puts "Forecast Cost in All Services: " + forecast_all.inject{ |sum, i| sum + i }.to_s
