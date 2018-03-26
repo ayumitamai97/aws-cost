df4ef6d1298e2a9a8ca7e38bd0d84fa8077e7499
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-26 13:17:35 +0900

    end_dayをDate.todayに修正

--- costexplorer.rb
+++ costexplorer.rb
@@ -10,26 +10,30 @@ Aws.config.update({
 })
 
 start_day = Date.today.to_s.slice(0,8) + "01" # 月初
-end_day = (Date.today - 1).to_s # 実行日の前日
+end_day = Date.today.to_s # 実行日の前日までのコストは、AWSではDate.todayまででよい
+# ∵ end_dayを本日としてdailyコストを取得した場合、
+#   各daily costが「1~2日」=1日の間、「2~3日」=2日、……「前日〜本日」=前日の間 となる
 
 ce = Aws::CostExplorer::Client.new
 
-resp = ce.get_dimension_values({
-  # search_string: "",
-  time_period: { # required
-    start: start_day, # required
-  end: end_day, # required
-  },
-  dimension: "SERVICE", # required, accepts AZ, INSTANCE_TYPE, LINKED_ACCOUNT, OPERATION, PURCHASE_TYPE, REGION, SERVICE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY
-  context: "COST_AND_USAGE", # accepts COST_AND_USAGE, RESERVATIONS
-  # next_page_token: "NextPageToken",
-})
-values = []
-for num in 0..30
-   values << resp[0][num].value
-   # puts resp[0][num].value
-end
-# p values
+# services = ce.get_dimension_values({
+#   # search_string: "",
+#   time_period: { # required
+#     start: start_day, # required
+#   end: end_day, # required
+#   },
+#   dimension: "SERVICE", # required, accepts AZ, INSTANCE_TYPE, LINKED_ACCOUNT, OPERATION, PURCHASE_TYPE, REGION, SERVICE, USAGE_TYPE, USAGE_TYPE_GROUP, RECORD_TYPE, OPERATING_SYSTEM, TENANCY, SCOPE, PLATFORM, SUBSCRIPTION_ID, LEGAL_ENTITY_NAME, DEPLOYMENT_OPTION, DATABASE_ENGINE, CACHE_ENGINE, INSTANCE_TYPE_FAMILY
+#   context: "COST_AND_USAGE", # accepts COST_AND_USAGE, RESERVATIONS
+#   # next_page_token: "NextPageToken",
+# })
+#
+# values = []
+#
+# for num in 0..30
+#    values << resp[0][num].value
+#    # puts resp[0][num].value
+# end
+
 resp = ce.get_cost_and_usage(params={
   time_period: {
     start: start_day, # required
@@ -74,7 +78,7 @@ costs = []
 for service in 0..29
   struct = resp.results_by_time[0]["groups"][service]
   cost = struct["metrics"]["BlendedCost"].amount.to_i
-  # puts struct.keys[0] + ": " + cost
+  puts struct.keys[0] + ": " + cost.to_s
   costs << cost
 end
 p costs.inject{ |sum, i| sum + i }
