02a8d7867e7921e92c7298bf401a210fbaa4a357
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-04-09 13:36:17 +0900

    forecast精度向上、ElastiCache出し分け途中

--- costexplorer.rb
+++ costexplorer.rb
@@ -6,7 +6,6 @@ require "pry"
 require "expanded_date"
 require_relative "aws_config"
 require_relative "spreadsheet"
-
 include AwsConfig
 include SpreadSheet
 include Authorize
@@ -14,77 +13,104 @@ include Authorize
 Authorize.authorize
 
 AwsConfig.ce_client
-ce = Aws::CostExplorer::Client.new
 
-start_day = Date.today.to_s.slice(0, 8) + "01" # 月初
-end_day = Date.today.to_s # 実行日の前日までのコストは、AWSではDate.todayまででよい
-# ∵ end_dayを本日としてdailyコストを取得した場合、
-#   各daily costが「1~2日」=1日の間、「2~3日」=2日、……「前日〜本日」=前日の間 となる
-past_days = end_day.slice(8, 9).to_i - start_day.slice(8, 9).to_i # 月初から前日までの日数
-last_day = Date.today.end_of_month.mday # 今月末の日付 (integerなので注意)
+monthly_values = ["Amazon Simple Storage Service", "Amazon EC2 Container Registry (ECR)", "Amazon CloudSearch", "Amazon Relational Database Service", "Amazon DynamoDB", "Amazon CloudFront", "Amazon ElastiCache", "AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"]
+# all services
 
-keys = ["Amazon Simple Storage Service", "Amazon EC2 Container Registry (ECR)", "Amazon CloudSearch", "Amazon Relational Database Service", "Amazon DynamoDB", "Amazon CloudFront", "Amazon ElastiCache"]
+daily_values = ["Amazon ElastiCache"] # 1日の大きいコストによって予報をブレさせないため
 
-responses = ce.get_cost_and_usage(
-  {
-    time_period: {
-      start: start_day, # required
-      end: end_day # required
-    },
-    granularity: "MONTHLY", # accepts DAILY, MONTHLY
-    filter: {
-      dimensions: {
-        key: "SERVICE",
-        values: ["Amazon Simple Storage Service", "Amazon EC2 Container Registry (ECR)", "Amazon CloudSearch", "Amazon Relational Database Service", "Amazon DynamoDB", "Amazon CloudFront", "Amazon ElastiCache", "AWS CloudTrail", "AWS CodeCommit", "AWS Config", "AWS Data Pipeline", "AWS Database Migration Service", "AWS Key Management Service", "AWS Lambda", "AWS Support (Business)", "Amazon API Gateway", "EC2 - Other", "Amazon Elastic Compute Cloud - Compute", "Amazon Elastic Load Balancing", "Amazon Elastic MapReduce", "Amazon Elasticsearch Service", "Amazon QuickSight", "Amazon Rekognition", "Amazon Route 53", "Amazon Simple Email Service", "Amazon Simple Notification Service", "Amazon Simple Queue Service", "Amazon SimpleDB", "Amazon Virtual Private Cloud", "AmazonCloudWatch", "Tax"] # all services
-      }
-    },
-    metrics: ["BlendedCost"],
-    group_by: [
-      {
-        type: "DIMENSION",
-        key: "SERVICE"
-      }
-    ]
-  }
-)
+def get_responses(granularity, values)
+  ce = Aws::CostExplorer::Client.new
 
-historical_all = [] # 全サービスのHistorical Total
-forecast_all = [] # 全サービスのForecast
+  start_day = Date.today.to_s.slice(0, 8) + "01" # 月初
+  end_day = Date.today.to_s # 実行日の前日までのコストは、AWSではDate.todayまででよい
+  # ∵ end_dayを本日としてdailyコストを取得した場合、
+  #   各daily costが「1~2日」=1日の間、「2~3日」=2日、……「前日〜本日」=前日の間 となる
+  past_days = end_day.slice(8, 9).to_i - start_day.slice(8, 9).to_i # 月初から前日までの日数
+  last_day = Date.today.end_of_month.mday # 今月末の日付 (integerなので注意)
 
-puts responses.results_by_time
+  keys = [ "Amazon Simple Storage Service", # 0
+    "Amazon Elastic Block Store", # 1 # EC2-Otherにあたる # => "EC2"
+    "Amazon Elastic Compute Cloud - Compute", # 2 # => "EC2"
+    "Amazon Elastic Load Balancing", # 3 # => "EC2"
+    "Amazon CloudSearch", # 4
+    "Amazon Relational Database Service",# 5
+    "Amazon DynamoDB", # 6
+    "Amazon CloudFront", # 7
+    "Amazon ElastiCache" ] # 8
 
-responses.results_by_time[0]["groups"].each do |struct| # struct は object "Aws::CostExplorer::Types::GetDimensionValuesResponse"
-  historical = struct["metrics"]["BlendedCost"].amount.to_f # 各サービスのHistorical Total
-  puts "Historical Total: " + struct.keys[0] + ": " + historical.to_s
-
-  forecast = historical * (last_day - past_days) / past_days
-
-  # このkeyをうまく利用したい…
-  keys.each do |key|
-    puts "Forecast Total: " + struct.keys[0] + ": " + forecast.to_s if
-     struct.keys[0] == key # 確認用
-  end
-
-  historical_all << historical
-  forecast_all << forecast
+  responses = ce.get_cost_and_usage(
+    {
+      time_period: {
+        start: start_day, # required
+        end: end_day # required
+      },
+      granularity: granularity, # accepts DAILY, MONTHLY
+      filter: {
+        dimensions: {
+          key: "SERVICE",
+          values: values
+        }
+      },
+      metrics: ["BlendedCost"],
+      group_by: [
+        {
+          type: "DIMENSION",
+          key: "SERVICE"
+        }
+      ]
+    }
+  )
 end
 
-forecast_all_total = forecast_all.inject { |sum, i| sum + i }.to_s
-forecast_ec2 = forecast_all[14] + forecast_all[15] + forecast_all[16]
+monthly_responses = get_responses("MONTHLY", monthly_values)
+daily_responses = get_responses("DAILY", daily_values)
+binding.pry
 
-forecast_selected = [
-  forecast_all[26], # S3
-  forecast_ec2, # ECR
-  forecast_all[10], # CloudSearch
-  forecast_all[21], # RDS
-  forecast_all[11], # DynamoDB
-  forecast_all[9], # CloudFront
-  forecast_all[13], # ElastiCache
-  "", # Other
-  forecast_all_total # SUM
-]
+structs = monthly_responses.results_by_time[0]["groups"]
 
-puts "Historical Total Cost in All Services: " + historical_all.inject { |sum, i| sum + i }.to_s
-puts "Forecast Cost in All Services: " + forecast_all_total
+historicals_all = [] # 全サービスのHistorical Total
+forecasts_particular = [] # "n月"シートに入力するコストの合計
+forecasts_all = [] # 利用サービス全てのコストの合計
 
-SpreadSheet.ce_on_ss(forecast_selected) # スプレッドシートにコスト予報を出力
+# puts monthly_responses.results_by_time
+
+keys.each do |key|
+  structs.each do |struct|
+    # struct は object "Aws::CostExplorer::Types::GetDimensionValuesResponse"
+
+    historical = struct["metrics"]["BlendedCost"].amount.to_f
+    # puts "Historical Total: " + struct.keys[0] + ": " + historical.to_s
+    # 各サービスのHistorical Total
+
+    forecast_particular = (historical * (last_day - past_days) / past_days) if
+     struct.keys[0] == key # 確認用
+
+    historicals_all << historical
+    forecasts_particular << forecast_particular.to_s
+
+  end
+end
+
+# p forecasts_particular
+
+structs.each do |struct|
+  historical = struct["metrics"]["BlendedCost"].amount.to_f
+  forecast_all = (historical * (last_day - past_days) / past_days)
+  forecasts_all << forecast_all
+end
+
+forecasts_particular = forecasts_particular.reject!{ |e| e.empty? } # 二重のループでできてしまった空文字を取り除く
+
+forecast_ec2 = forecasts_particular[2].to_f + forecasts_particular[3].to_f + forecasts_particular[4].to_f
+forecasts_particular.slice!(1..3) # 2番目から要素3つ削除
+forecasts_particular.insert(1, forecast_ec2) # 2番目にforecast_ec2(計算結果)を追加
+forecasts_particular.insert(-1, "") # 末尾に空白(Otherの部分)を追加
+
+forecasts_all_sum = forecasts_all.inject { |sum, i| sum + i }.to_s
+forecasts_particular.insert(-1, forecasts_all_sum) # 末尾にforecasts_all_sum(計算結果)を追加
+
+puts "Historical Total Cost in All Services: " + historicals_all.inject { |sum, i| sum + i }.to_s
+puts "Forecast Cost in All Services: " + forecasts_all_sum
+
+SpreadSheet.ce_on_ss(forecasts_particular) # スプレッドシートにコスト予報を出力
